6b9bc9b4781c5b69b2982b0e7148d1ea06b48381
Vladislav Astapenko <vsastapenko@gmail.com>
2021-10-30 17:11:49 +0300

    final fix of homework 6, works good

--- Homework6/factory/NeutralFactory.java
+++ Homework6/factory/NeutralFactory.java
@@ -5,27 +5,27 @@ import com.company.homeworkSix.robot.RobotBodyParts;
 
 public class NeutralFactory implements Runnable {
 
-    private final Storage storage;
+    private final Storage NEUTRAL_STORAGE;
     private int count = 0;
     private final EndOfWarFlag END_OF_WAR;
 
-    public NeutralFactory(Storage storage) {
-        this.storage = storage;
-        this.END_OF_WAR = new EndOfWarFlag();
+    public NeutralFactory(Storage storage, EndOfWarFlag flag) {
+        this.NEUTRAL_STORAGE = storage;
+        this.END_OF_WAR = flag;
     }
 
     @Override
     public void run() {
         try {
             while (true) {
-                synchronized (storage.getBODY_PARTS_LIST()) {
+                synchronized (NEUTRAL_STORAGE.getBODY_PARTS_LIST()) {
                     if (!END_OF_WAR.checkIfWarIsStillGoing()) {
                         return;
                     }
                     count++;
 
-                    Thread.sleep(5000);
-                    storage.getBODY_PARTS_LIST().add(RobotBodyParts.randomPart());
+                    Thread.sleep(500);
+                    NEUTRAL_STORAGE.getBODY_PARTS_LIST().add(RobotBodyParts.randomPart());
                     System.out.println("Factory produced: " + count);
                 }
             }
